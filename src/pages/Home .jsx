5691863ab7ce59ca2f366a0c1fc8f4902fe3c356
jannat355 <kehindejannat@icloud.com>
2024-01-04 03:36:58 -0800

    restfullf

--- src/pages/Home .jsx
+++ src/pages/Home .jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import goalpic from '../assets/img2.jpeg'
 import goallogo from '../assets/img3.jpeg'
+import { Link } from 'react-router-dom'
 
 const Home  = () => {
   return (
@@ -13,8 +14,9 @@ const Home  = () => {
           <h2>Improve productivity by managing<span className='text-primary'> Your user</span></h2>
           <p className='py-4'>Lorem ipsum dolor sit amet consectetur. Ut nisl nisl cursus massa sed. Turpis ac aliquet lacinia justo turpis amet at arcu. Diam vulputate suspendisse aliquam enim sagittis cursiodio ultrices. Condimentum lacus nunc rhoncus massa. Tortorstiu ultricies neque aliquam sit non. Diam vehicula dignissimepei pellentesque eros vitae. Viverra in vitae nunc lorem eget lciou imperdiet tortor. Ac mauris vel non amet eget egestas inoriou pellentesque commodo amet. Facilisi sed ut nisi pellentesque diam egestas et turpis donor amet.</p>
           <div className='d-flex gap-5'>
-            <button className="btn btn-primary btn-lg">+ New User</button>
-          <button className=" btn btn-primary btn-lg">All Users</button>
+           <Link to='/NewUser'> <button className="btn btn-primary btn-lg">+ New User</button></Link>
+           <Link to='/AllUsers'> <button className="btn btn-primary btn-lg"> All User</button></Link>
+          
           </div>
         </div>
       </div>
